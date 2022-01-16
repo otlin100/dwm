8d8e6f9c275c43359bdce6fe4d05f6ec6dbf2b9f
Otto Bo-Shao Lin <ottolin@outlook.de>
2022-01-16 18:49:15 +0100

    new machine

--- config.h
+++ config.h
@@ -15,8 +15,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FuraMono Nerd Font:size=6", "JoyPixels:pixelsize=6:antialias=true:autohint=true"};
-static char dmenufont[]             = "monospace:size=6";
+static const char *fonts[]          = { "FuraMono Nerd Font:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
+static char dmenufont[]             = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
